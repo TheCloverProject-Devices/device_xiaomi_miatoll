e8d70ef57b5d0c296dc3ab5a145076896723a1fb
Ritesh <SahanyRitesh@proton.me>
2024-05-08 15:35:59 +0530

    miatoll: overlay: Show battery cycle count Signed-off-by: Ritesh <SahanyRitesh@proton.me>

--- atoll.mk
+++ atoll.mk
@@ -187,6 +187,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
+    MiatollSettings \
     SM6250Frameworks \
     SM6250SystemUI \
     TelephonyResCommon \
