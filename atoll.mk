ccb3d3ba6382190da5d82b5466e7d9a9ec5e7909
0xCAFEBABE <me.cafebabe@gmail.com>
2024-06-30 20:42:43 +0800

    miatoll: Setup DeviceAsWebcam
    Change-Id: Icf8e10cd21c4eef9a66af624971ffe9c4d39d992

--- atoll.mk
+++ atoll.mk
@@ -270,6 +270,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     MiatollSettings \
+    SM6250DeviceAsWebcam \
     SM6250Frameworks \
     SM6250SystemUI \
     TelephonyResCommon \
