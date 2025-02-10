3bf6bed4c490fb56484fc5e656a7097596d05ff6
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-02-10 10:19:43 +0000

    Merge `sm6250-common` into `miatoll`
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -4,7 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.file import File
 from extract_utils.fixups_blob import (
+    BlobFixupCtx,
     blob_fixup,
     blob_fixups_user_type,
 )
@@ -17,9 +19,15 @@ from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
+from extract_utils.tools import (
+    llvm_objdump_path,
+)
+from extract_utils.utils import (
+    run_cmd,
+)
 
 namespace_imports = [
-    'device/xiaomi/sm6250-common',
+    'device/xiaomi/miatoll',
     'hardware/qcom-caf/sm8150',
     'hardware/qcom-caf/wlan',
     'hardware/xiaomi',
@@ -47,6 +55,33 @@ lib_fixups: lib_fixups_user_type = {
         'libwpa_client',
     ): lib_fixup_remove,
 }
+
+
+def blob_fixup_return_1(
+    ctx: BlobFixupCtx,
+    file: File,
+    file_path: str,
+    symbol: str,
+    *args,
+    **kwargs,
+):
+    for line in run_cmd(
+        [
+            llvm_objdump_path,
+            '--dynamic-syms',
+            file_path,
+        ]
+    ).splitlines():
+        if line.endswith(f' {symbol}'):
+            offset, _ = line.split(maxsplit=1)
+
+            with open(file_path, 'rb+') as f:
+                f.seek(int(offset, 16))
+                f.write(b'\x01\x00\xa0\xe3')  # mov r0, #1
+                f.write(b'\x1e\xff\x2f\xe1')  # bx lr
+
+            break
+
 
 blob_fixups: blob_fixups_user_type = {
     'vendor/etc/camera/camxoverridesettings.txt': blob_fixup()
@@ -91,7 +126,7 @@ blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
 
 module = ExtractUtilsModule(
-    'sm6250-common',
+    'miatoll',
     'xiaomi',
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
