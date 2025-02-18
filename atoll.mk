1ad70df6759212ff710c89ee027189748b20de9b
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-02-18 20:55:44 +0000

    miatoll: Inherit private key repo.
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- atoll.mk
+++ atoll.mk
@@ -12,6 +12,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# Inherit the private key repository for signing the build
+-include vendor/aosp/keys/keys.mk
+
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
