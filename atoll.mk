35f054947f2fd627e9c1783591995ca9799b0b01
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-03-25 01:28:53 +0530

    miatoll: Added `Dolby regulator`
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- atoll.mk
+++ atoll.mk
@@ -94,6 +94,18 @@ PRODUCT_PACKAGES += \
 # Dolby
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
+# MotoDolbyDax3 Permissions & Sysconfig
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/dolby/system/etc/permissions/com.motorola.dolby.dolbyui.dax3.features.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.motorola.dolby.dolbyui.dax3.features.xml \
+    $(LOCAL_PATH)/dolby/system/etc/sysconfig/hiddenapi-whitelist-com.motorola.dolby.dolbyui.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-whitelist-com.motorola.dolby.dolbyui.xml
+# Motorola daxservice Permissions & Sysconfig
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/dolby/system_ext/etc/permissions/com.dolby.daxservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.dolby.daxservice.xml \
+    $(LOCAL_PATH)/dolby/system_ext/etc/sysconfig/hiddenapi-whitelist-com.dolby.daxservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/hiddenapi-whitelist-com.dolby.daxservice.xml
+# Build Motorola Dolby tuning
+PRODUCT_PACKAGES += \
+    daxService \
+    MotoDolbyDax3
 
 # DRM
 PRODUCT_PACKAGES += \
