1d1ceefa65500ed96ca2f2c13c6d7ca1178c47f1
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:37:50 +0100

    miatoll: Migrate Lineage Health to soong_config_set
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -116,9 +116,6 @@ TARGET_KERNEL_CLANG_COMPILE := true
 KERNEL_CC := CC=clang
 override KERNEL_TOOLCHAIN_PREFIX_arm := arm-linux-android-
 
-# Lineage Health
-TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
-
 # Media
 TARGET_USES_ION := true
 
