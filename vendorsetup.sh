4935d6f8af18af25ab9f5806cef718fdee5885ea
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-08-08 16:34:38 +0000

    miatoll: Add vendorsetup.sh
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# hardware/sony/timekeep
+git clone -b lineage-22.2 https://github.com/LineageOS/android_hardware_sony_timekeep.git hardware/sony/timekeep --depth=1
+
+# hardware/xiaomi
+git clone -b lineage-22.2 https://github.com/LineageOS-miatoll/hardware_xiaomi.git hardware/xiaomi --depth=1
+
+# Vendor tree
+git clone -b clover-16 https://github.com/TheCloverProject-Devices/vendor_xiaomi_miatoll.git vendor/xiaomi/miatoll --depth=1
+
+# Kernel tree
+git clone -b clover-16 https://github.com/TheCloverProject-Devices/kernel_xiaomi_sm6250.git kernel/xiaomi/sm6250 --depth=1
