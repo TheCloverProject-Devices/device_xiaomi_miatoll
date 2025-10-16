9492e72e44adfbdb4244569b51125ca0f8b5b28c
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-10-16 13:51:47 +0000

    miatoll: Build android.hardware.bluetooth(audio/a2dp)
    2025-10-16 18:34:35.505   594-594   hwservicemanager        pid-594                              I  getTransport: Cannot find entry android.hardware.bluetooth.audio@2.2::IBluetoothAudioProvidersFactory/default in either framework or device VINTF manifest.
    2025-10-16 18:34:35.505 20028-20028 LegacySupport           pid-20028                            E  Could not get passthrough implementation for android.hardware.bluetooth.audio@2.2::IBluetoothAudioProvidersFactory/default.
    2025-10-16 18:34:35.506   594-594   hwservicemanager        pid-594                              I  getTransport: Cannot find entry android.hardware.bluetooth.audio@2.1::IBluetoothAudioProvidersFactory/default in either framework or device VINTF manifest.
    2025-10-16 18:34:35.508 20028-20028 LegacySupport           pid-20028                            E  Could not get passthrough implementation for android.hardware.bluetooth.audio@2.1::IBluetoothAudioProvidersFactory/default.
    2025-10-16 18:34:35.508   594-594   hwservicemanager        pid-594                              I  getTransport: Cannot find entry android.hardware.bluetooth.audio@2.0::IBluetoothAudioProvidersFactory/default in either framework or device VINTF manifest.
    2025-10-16 18:34:35.508 20028-20028 LegacySupport           pid-20028                            E  Could not get passthrough implementation for android.hardware.bluetooth.audio@2.0::IBluetoothAudioProvidersFactory/default.
    2025-10-16 18:34:35.508 20028-20028 audiohalservice         pid-20028                            W  Could not register Bluetooth Audio API
    2025-10-16 18:34:35.509   594-594   hwservicemanager        pid-594                              I  getTransport: Cannot find entry android.hardware.bluetooth.a2dp@1.0::IBluetoothAudioOffload/default in either framework or device VINTF manifest.
    2025-10-16 18:34:35.509 20028-20028 LegacySupport           pid-20028                            E  Could not get passthrough implementation for android.hardware.bluetooth.a2dp@1.0::IBluetoothAudioOffload/default.
    
    Change-Id: I758baab9544ff0a155bb10fef24c6c817729ad19
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- atoll.mk
+++ atoll.mk
@@ -46,7 +46,19 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
+    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.0.vendor \
+    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio@2.1.vendor \
+    android.hardware.bluetooth.audio@2.2-impl \
+    android.hardware.bluetooth.audio@2.2.vendor \
     android.hardware.soundtrigger@2.3-impl
+
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.a2dp@1.0 \
+    android.hardware.bluetooth.a2dp@1.0.vendor \
+    android.hardware.bluetooth.a2dp@1.0-impl \
+    android.hardware.bluetooth.a2dp@1.0-service
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
